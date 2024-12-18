3e9043d5606b385703c2cb383d3dfdb6992448a9
Daniel Mazzeu <danmazzeu9@gmail.com>
2024-12-18 13:17:48 -0300

    Update

---
+++ Module-02/TypeAnnotation/app.ts
@@ -0,0 +1,17 @@
+// Variables
+let fullname: string = 'Daniel Mazzeu';
+
+// Arrays
+let animals: string[] = ['Elephant', 'Dog', 'car', 'Panda', 'Giraffe'];
+
+// Objects
+let car: {
+    name: string;
+    year: number;
+    price: number;
+};
+
+// Functions with Type Annotations
+function multiplyNumbers(num1: number, num2: number): number {
+    return num1 * num2;
+}
